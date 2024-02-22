2077c002497c641f19e09c71f177166f6c701845
Little_Q <1179177709@qq.com>
2024-02-22 16:25:11 +0800

    修改

--- src/components/Scene7_gltf.vue
+++ src/components/Scene7_gltf.vue
@@ -19,7 +19,7 @@ let camera = null;
 let controls = null;
 
 let mesh = null;
-
+let gltfGroup = null;
 
 //初始化
 const init = () => {
@@ -31,6 +31,7 @@ const init = () => {
         antialias: true // 抗锯齿
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.outputEncoding = THREE.sRGBEncoding;
     // 允许在场景中使用阴影贴图
     renderer.shadowMap.enabled = true;
     addControl();
@@ -49,7 +50,7 @@ const addControl = () => {
 }
 
 const addGridMesh = () => {
-    const gridHelper = new THREE.GridHelper(10, 10);
+    const gridHelper = new THREE.GridHelper(16, 16);
     gridHelper.material.opacity = 0.2;
     gridHelper.material.transparent = true;
     scene.add(gridHelper);
@@ -65,28 +66,38 @@ const addMesh = () => {
 
 }
 
-const addGltf = () => {
+const addGltf = async () => {
     const gltfLoader = new GLTFLoader().setPath('./model/');
 
     const dracoLoader = new DRACOLoader(); // 压缩glb文件
-    dracoLoader.setDecoderPath("./draco/"); //设置解压库文件路径
+    dracoLoader.setDecoderPath("./draco/gltf/"); //设置解压库文件路径
     dracoLoader.setDecoderConfig({ type: "js" }); //使用js方式解压
     dracoLoader.preload(); //初始化_initDecoder 解码器
 
     gltfLoader.setDRACOLoader(dracoLoader);
 
-    gltfLoader.load("city_test.glb", (gltf) => {
+    const gltf = await gltfLoader.loadAsync('city_test.glb');
+    let city = gltf.scene;
+    gltfGroup = city;
+    console.log(gltf);
+    city.castShadow = true;
+    city.position.set(-20, 5, -10);
+    city.scale.set(0.0001, 0.0001, 0.0001);
+    scene.add(city);
 
-        let city = gltf.scene;
-        console.log(city);
-        city.position.set(0, 0, 0);
-        scene.add(city);
-    })
+    // gltfLoader.load("city_test.glb", (gltf) => { })
 }
 
 const addLight = () => {
     const light = new THREE.AmbientLight(0x404040); // 柔和的白光
     scene.add(light);
+
+    // 添加一个平行光源
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(100, 100, 100);
+    // 此属性设置为 true 灯光将投射阴影
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
 }
 
 const addGui = () => {
@@ -95,16 +106,62 @@ const addGui = () => {
     gui.addColor(mesh.material, 'color').name('更改物体颜色');
 }
 
+window.addEventListener("onresize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+window.addEventListener('click', (event) => {
+    event.preventDefault();
+    let rayCaster = new THREE.Raycaster();
+    let mouse = new THREE.Vector2();
+
+    //通过鼠标点击位置，计算出raycaster所需点的位置，以屏幕为中心点，范围-1到1
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    //通过鼠标点击的位置(二维坐标)和当前相机的矩阵计算出射线位置
+    rayCaster.setFromCamera(mouse, camera);
+
+
+    let arr = [];
+    gltfGroup.traverse(function (obj) {
+        if (obj.isMesh) {//判断是否是网格模型
+            arr.push(obj);
+        }
+    });
+    //获取与射线相交的对象数组， 其中的元素按照距离排序，越近的越靠前。
+    //+true，是对其后代进行查找，这个在这里必须加，因为模型是由很多部分组成的，后代非常多。
+    let intersects = rayCaster.intersectObjects(arr, true);
+
+
+    if (intersects.length > 0) {
+        const material = new THREE.MeshStandardMaterial({
+            color: 0xffffff * Math.random(),
+            opacity: 0.8
+        });
+        // intersects[0].object.material = material;
+        intersects.forEach((item) => {
+            if (item.object.isMesh) {//判断是否是网格模型
+                console.log('模型节点名字', item.object.name);
+                item.object.material = material;
+            }
+        })
+    }
+
+
+});
 
 
 onMounted(() => {
     init();
     Contanier3d.value.appendChild(renderer.domElement);
 
-    renderer.setClearColor(0x000000);
+    renderer.setClearColor(0xffffff);
 
-    scene.background = new THREE.Color(0x444444);
-    scene.envirment = new THREE.Color(0x444444);
+    // scene.background = new THREE.Color(0x444444);
+    // scene.envirment = new THREE.Color(0x444444);
 
     render();
 
